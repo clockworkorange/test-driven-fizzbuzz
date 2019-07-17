34f5ffbd46b9caf80bf241765199137afcded654
Julio Ripoll Moreno <julio@ripollmoreno.com>
2019-07-17 14:40:51 +0200

    Added multiple of 3 and 5

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 module.exports = (num) => {
-    if(num % 15 === 0) return 'FizzBuzz';
+    if(multipleOf3(num) && !multipleOf5(num)) {
+        return 'Fizz'
+    }
+    if(!multipleOf3(num) && multipleOf5(num)) {
+        return 'Buzz'
+    }
+    if(multipleOf15(num)) return 'FizzBuzz';
     return `${num}`
 };
+
+const multipleOf3 = number => (number % 3 === 0);
+const multipleOf5 = number => (number % 5 === 0);
+const multipleOf15 = number => (number % 15 === 0);
--- index.test.js
+++ index.test.js
@@ -11,4 +11,16 @@ describe('fizzBuzz()', () => {
         expect(fizzBuzz(1)).toBe('1');
         expect(fizzBuzz(22)).toBe('22');
     });
+
+    it('returns "Fizz" for multiples of 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz');
+        expect(fizzBuzz(6)).toBe('Fizz');
+        expect(fizzBuzz(9)).toBe('Fizz');
+    });
+
+    it('returns "Buzz" for multiples of 5', () => {
+        expect(fizzBuzz(5)).toBe('Buzz');
+        expect(fizzBuzz(10)).toBe('Buzz');
+        expect(fizzBuzz(25)).toBe('Buzz');
+    });
 });
